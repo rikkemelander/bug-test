482934dec813a675faf646dd1ba7ac673dd08f48
Admin Jensen <admin@wayfare.ai>
2025-06-17 10:41:56 +0200

    test

--- models/python_model.py
+++ models/python_model.py
@@ -2,5 +2,5 @@ def model(dbt, session):
     dbt.config(
         materialized="table"
     )
-    df = session.sql(f"SELECT * FROM {ref('AGGREGATES')}").to_pandas()
+    df = session.sql(f"SELECT * FROM {{ ref('AGGREGATES') }}").to_pandas()
     return df
