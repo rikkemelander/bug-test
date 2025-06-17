ed831ab131a563f0284cd9b34c48965ac93a0b07
Admin Jensen <admin@wayfare.ai>
2025-06-17 10:49:00 +0200

    test

--- models/python_model.py
+++ models/python_model.py
@@ -2,5 +2,5 @@ def model(dbt, session):
     dbt.config(
         materialized="table"
     )
-    df = session.sql(f"SELECT * FROM {ref('aggregates')}").to_pandas()
+    df = session.sql(f"SELECT * FROM { source('sync', 'PRODUCTS') }").to_pandas()
     return df
